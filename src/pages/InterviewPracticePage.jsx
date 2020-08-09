c8153af74ec747928438c4d07995bcdf479516b1
morykwon <kwonmory@gmail.com>
2020-08-09 13:41:18 +0900

    Clean up unnecessary code

--- src/pages/InterviewPracticePage.jsx
+++ src/pages/InterviewPracticePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import InterviewCategoriesContainer from '../containers/InterviewCategoriesContainer';
 import InterviewQuestionsContainer from '../containers/InterviewQuestionsContainer';
