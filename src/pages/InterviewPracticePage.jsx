4dfed468e18d609305ca3afc27855b064af1c6ca
morykwon <kwonmory@gmail.com>
2020-08-09 13:37:40 +0900

    Move state on the page component to another component

--- src/pages/InterviewPracticePage.jsx
+++ src/pages/InterviewPracticePage.jsx
@@ -1,29 +1,13 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
-
 import InterviewCategoriesContainer from '../containers/InterviewCategoriesContainer';
 import InterviewQuestionsContainer from '../containers/InterviewQuestionsContainer';
 
-import { loadQuestions, loadParts } from '../modules/reducer';
-
-const InterviewPracticePage = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadParts());
-  }, []);
-
-  useEffect(() => {
-    dispatch(loadQuestions());
-  }, []);
-
-  return (
-    <>
-      <InterviewCategoriesContainer />
-      <InterviewQuestionsContainer />
-    </>
-  );
-};
+const InterviewPracticePage = () => (
+  <>
+    <InterviewCategoriesContainer />
+    <InterviewQuestionsContainer />
+  </>
+);
 
 export default InterviewPracticePage;
--- src/pages/InterviewPracticePage.test.jsx
+++ src/pages/InterviewPracticePage.test.jsx
@@ -4,13 +4,7 @@ import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
 
-import { useDispatch, useSelector } from 'react-redux';
-
 import InterviewPracticePage from './InterviewPracticePage';
-import interviewQuestions from '../../fixtures/interview-questions';
-
-import { interviewQuestionsTerms } from '../../fixtures/term';
-import interviewParts from '../../fixtures/parts';
 
 jest.mock('react-redux');
 
@@ -23,42 +17,7 @@ function renderInterviewListPage() {
 }
 
 describe('InterviewQuestionPage', () => {
-  const dispatch = jest.fn();
-
-  beforeEach(() => {
-    dispatch.mockClear();
-
-    useDispatch.mockImplementation(() => dispatch);
-
-    useSelector.mockImplementation((selector) => selector({
-      interview: {
-        questions: given.questions,
-        parts: interviewParts,
-      },
-    }));
-  });
-
-  it('calls questions data and parts data', () => {
+  it('renders InterviewPracticePage ', () => {
     renderInterviewListPage();
-
-    expect(dispatch).toBeCalledTimes(2);
-  });
-
-  context('with interview questions', () => {
-    given('questions', () => interviewQuestions);
-    it('renders interview questions', () => {
-      const { container } = renderInterviewListPage();
-
-      expect(container).not.toHaveTextContent(interviewQuestionsTerms.emptyMessage);
-    });
-  });
-
-  context('without interview questions', () => {
-    given('questions', () => []);
-    it('renders questions empty messages', () => {
-      const { container } = renderInterviewListPage();
-
-      expect(container).toHaveTextContent(interviewQuestionsTerms.emptyMessage);
-    });
   });
 });
