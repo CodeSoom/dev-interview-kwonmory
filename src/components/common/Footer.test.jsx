c07b3dbf3ac4d159e09b5b3641f3af34e2481808
Mory Kwon <morykwon@Moryui-MacBookPro.local>
2020-09-04 17:59:30 +0900

    add footer

---
+++ src/components/common/Footer.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Footer from './Footer';
+
+describe('Footer', () => {
+  it('renders Footer', () => {
+    render(<Footer />);
+  });
+});
--- src/layout/BaseLayout.jsx
+++ src/layout/BaseLayout.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import { useSelector } from 'react-redux';
+
 import HeaderContainer from '../containers/common/HeaderContainer';
+import Footer from '../components/common/Footer';
 
 import Loadding from '../components/common/Loading';
 
 import { get } from '../utils';
 
 const Wrapper = styled.div({
-
+  // empty
 });
 
 const BaseLayout = ({
@@ -25,6 +27,7 @@ const BaseLayout = ({
       <Wrapper>
         <HeaderContainer blue={blue} />
         {children}
+        <Footer />
       </Wrapper>
     </>
   );
