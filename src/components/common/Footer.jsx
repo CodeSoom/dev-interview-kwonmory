c37475f2a027a90371aad49e280a3ad56ffc248a
Mory Kwon <morykwon@Moryui-MacBookPro.local>
2020-09-08 17:52:30 +0900

    Add github icon in footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,14 +4,15 @@ import styled from '@emotion/styled';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHippo } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const FooterStyled = styled.div({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  height: '3rem',
+  height: '4rem',
   color: 'rgba(255, 255, 255, .9)',
-  fontSize: '.8rem',
+  fontSize: '1rem',
   backgroundImage: 'linear-gradient(128deg,#363B3E 6%,#363B3E 91%)',
 
   span: {
@@ -19,6 +20,9 @@ const FooterStyled = styled.div({
     a: {
       color: 'rgba(255, 255, 255, .9)',
       textDecoration: 'none',
+      svg: {
+        fontSize: '1.3rem',
+      },
     },
   },
 });
@@ -29,11 +33,14 @@ const Footer = () => (
       <span>
         2020 ⓒ
         {' '}
-        <a href="https://github.com/CodeSoom/check-your-self-kwonmory" target="_blank" rel="noreferrer">체크유얼셀프</a>
         {' '}
-        |
+        <a href="https://github.com/CodeSoom/check-your-self-kwonmory" target="_blank" rel="noreferrer">
+          체크유얼셀프 |
+          {' '}
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
       </span>
-      <FontAwesomeIcon icon={faHippo} />
+
     </FooterStyled>
   </>
 );
